b236e2e9cc33a6316c0aa0e55b868079053dd37d
Guillermo Rubilar <gfrubi@gmail.com>
2017-05-29 17:02:12 -0400

    actualizado codigo graficos Bessel

--- figuras-editables/fig-Bessel.py
+++ figuras-editables/fig-Bessel.py
@@ -71,15 +71,11 @@ savefig('../figs/fig-Bessel-K.pdf')
 
 # #$j_{n}(x)$:
 
-def fun_sph_jn(n,z):
-    return sph_jn(n,z)[0][n]
-fun_sph_jn= vectorize(fun_sph_jn)
-
-x = linspace(-10,10,1000)
+x = linspace(0,10,1000)
 
 fig = figure(figsize=(8,6))
 for n in range(5):
-    plot(x,fun_sph_jn(n,x),colores[n], dashes=dasheses[n],label='$n= $'+str(n), linewidth=2)
+    plot(x,spherical_jn(n,x),colores[n], dashes=dasheses[n],label='$n= $'+str(n), linewidth=2)
 grid()
 xlabel(r'$x$',fontsize=15)
 ylabel(r'$j_n(x)$',fontsize=15)
@@ -91,14 +87,10 @@ savefig('../figs/fig-Bessel-Esferica-j.pdf')
 
 # #$n_{n}(x)$:
 
-def fun_sph_yn(n,z):
-    return sph_yn(n,z)[0][n]
-fun_sph_yn= vectorize(fun_sph_yn)
-
 x = linspace(-20,20,1000)
 fig = figure(figsize=(8,6))
 for n in range(5):
-    plot(x,fun_sph_yn(n,x),colores[n], dashes=dasheses[n],label='$n= $'+str(n), linewidth=2)
+    plot(x,spherical_yn(n,x),colores[n], dashes=dasheses[n],label='$n= $'+str(n), linewidth=2)
 grid()
 xlabel(r'$x$',fontsize=15)
 ylabel(r'$n_n(x)$',fontsize=15)
@@ -110,38 +102,28 @@ savefig('../figs/fig-Bessel-Esferica-n.pdf')
 
 # #$i_{n}(x)$:
 
-def fun_sph_in(n,z):
-    return sph_in(n,z)[0][n]
-fun_sph_in= vectorize(fun_sph_in)
-
-x = linspace(-5,5,1000)
+x = linspace(0,5,1000)
 fig = figure(figsize=(8,6))
 for n in range(5):
-    plot(x,fun_sph_in(n,x),colores[n], dashes=dasheses[n],label='$n= $'+str(n), linewidth=2)
+    plot(x,spherical_in(n,x),colores[n], dashes=dasheses[n],label='$n= $'+str(n), linewidth=2)
 grid()
 xlabel(r'$x$',fontsize=15)
 ylabel(r'$i_n(x)$',fontsize=15)
 #title(ur'Funciones Esféricas Modificadas de Bessel de $1^{\circ}$ especie a orden entero',fontsize=12)
 legend(loc='best',fontsize=12)
-xlim(-5,5)
-ylim(-10,10)
 savefig('../figs/fig-Bessel-Esferica-i.pdf')
 
 
 # #$k_{n}(x)$:
 
-def fun_sph_kn(n,z):
-    return sph_kn(n,z)[0][n]
-fun_sph_kn= vectorize(fun_sph_kn)
 
-x = linspace(-5,5,1000)
+x = linspace(5,1000)
 fig = figure(figsize=(8,6))
 for n in range(5):
-    plot(x,fun_sph_kn(n,x),colores[n], dashes=dasheses[n],label='$n= $'+str(n), linewidth=2)
+    plot(x,spherical_kn(n,x),colores[n], dashes=dasheses[n],label='$n= $'+str(n), linewidth=2)
 grid()
 xlabel(r'$x$',fontsize=15)
 ylabel(r'$k_n(x)$',fontsize=15)
 #title(ur'Funciones Esféricas Modificadas de Bessel de $2^{\circ}$ especie a orden entero',fontsize=12)
 legend(loc='best',fontsize=12)
-xlim(-5,5)
-ylim(-15,15)
+ylim(-1,15)
