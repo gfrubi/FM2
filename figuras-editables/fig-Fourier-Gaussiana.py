d6dea09a7ca231e153de57739952faad652d86c4
Guillermo Rubilar <gfrubi@gmail.com>
2019-04-05 17:11:45 -0300

    nuevos links a código de gráficos de T. de Fourier

--- figuras-editables/fig-Fourier-Gaussiana.py
+++ figuras-editables/fig-Fourier-Gaussiana.py
@@ -1,22 +1,22 @@
 # -*- coding: utf-8 -*-
 
-from matplotlib.pyplot import *
-from numpy import *
-style.use('classic')
-x=linspace(-10,10,1000)
-k=linspace(-10,10,1000)
-alpha=1
-figure(figsize=(15,5))
-subplot(1,2,1)
-plot(x,exp(-alpha*x**2),lw=2,label=r'$e^{-t^2}$')
-grid()
-legend(loc='best', fontsize=15)
-xlabel(r'$t$',fontsize=15)
-ylabel(r'$f(t)$',fontsize=15)
-subplot(1,2,2)
-plot(k,sqrt(pi)*exp(-k**2/(4.*alpha)),lw=2,label=r'$\sqrt{\pi}e^{-\omega^2/4}$')
-grid()
-legend(loc='best',fontsize=15)
-xlabel(r'$\omega$',fontsize=15)
-ylabel(r'$\tilde{f}(\omega)$',fontsize=15)
-savefig("../figs/fig-Fourier-Gaussiana.pdf")
+import matplotlib.pyplot as plt
+import numpy as np
+plt.style.use('classic')
+x = np.linspace(-10,10,1000)
+k = np.linspace(-10,10,1000)
+alpha = 1
+plt.figure(figsize=(15,5))
+plt.subplot(1,2,1)
+plt.plot(x,np.exp(-alpha*x**2),lw=2,label='$e^{-t^2}$')
+plt.grid(True)
+plt.legend(loc='best', fontsize=15)
+plt.xlabel('$t$',fontsize=15)
+plt.ylabel('$f(t)$',fontsize=15)
+plt.subplot(1,2,2)
+plt.plot(k,np.sqrt(np.pi)*np.exp(-k**2/(4.*alpha)),lw=2,label='$\sqrt{\pi}e^{-\omega^2/4}$')
+plt.grid(True)
+plt.legend(loc='best',fontsize=15)
+plt.xlabel('$\omega$',fontsize=15)
+plt.ylabel('$\tilde{f}(\omega)$',fontsize=15)
+plt.savefig("../figs/fig-Fourier-Gaussiana.pdf")
