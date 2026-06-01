c31e30bdb34832b9dc4ff371a0ca1e707a581366
Guillermo Rubilar <gfrubi@gmail.com>
2026-06-01 19:14:49 -0400

    actualizado código

--- figuras-editables/Graficos-AE-esfera-colores.py
+++ figuras-editables/Graficos-AE-esfera-colores.py
@@ -3,35 +3,33 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import cm, colors
-#style.use('classic')
-plt.rc('text', usetex=True)
+plt.style.use('classic')
 
-t,p = np.meshgrid(np.linspace(0,np.pi,200),np.linspace(0,2*np.pi,200)) #arrays de variables angulares
+th,ph = np.meshgrid(np.linspace(0,np.pi,200),np.linspace(0,2*np.pi,200)) #arrays de variables angulares
 
 def X(R):
-    return R*np.sin(t)*np.cos(p)
+    return R*np.sin(th)*np.cos(ph)
 
 def Y(R):
-    return R*np.sin(t)*np.sin(p)
+    return R*np.sin(th)*np.sin(ph)
 
 def Z(R):
-    return R*np.cos(t)
+    return R*np.cos(th)
 
 nmax = 5
 
 fig = plt.figure(figsize=(3*nmax,3*nmax))
 for n in range(nmax+1):
     for m in range(n+1):
-        #print(n,m)
+        print(n,m)
         ax = fig.add_subplot(nmax+1, nmax+1, (nmax+1)*n+1+m, projection='3d')
-        C = sp.sph_harm(m, n, p, t).real
+        f = sp.sph_harm_y(n, m, th, ph).real # evalúa la parte real del armónico esférico
         norm = colors.Normalize()
-        ax.plot_surface(X(1),Y(1),Z(1),rstride=1, cstride=1,norm=norm,cmap=cm.jet,facecolors=cm.jet(norm(C)))
+        ax.plot_surface(X(1),Y(1),Z(1),rstride=1, cstride=1,norm=norm,cmap=cm.jet,facecolors=cm.jet(norm(f)))
         ax.set_title('$n = %d, m= %d$'%(n,m), fontsize=20)
-        #ax.set_frame_on(False)
         ax.set_axis_off()
-        cosa = cm.ScalarMappable(cmap=cm.jet)
-        cosa.set_array(C)
+        colores = cm.ScalarMappable(cmap=cm.jet)
+        colores.set_array(f)
         ax.set_xlim(-1,1)
         ax.set_ylim(-1,1)
         ax.set_zlim(-1,1)
